cd15facab15886d7a329b9c8c5ee0cc3f7f5a76c
duellal <alexandriaduell@gmail.com>
2022-03-06 15:13:35 -0500

    Solved first 2 functions

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-  /*add your code here*/
+function multiply(a, b){
+  return a * b;
 }
 
 
@@ -77,8 +77,8 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7
 }
 
 
